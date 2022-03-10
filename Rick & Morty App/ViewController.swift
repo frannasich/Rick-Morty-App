15a2cfe2e77b41d5f6fc21ec88ad44b73cdb5fa4
Francisco Nasich <frannasich@gmail.com>
2022-03-10 12:17:59 -0300

    Initial Commit

---
+++ Rick & Morty App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick & Morty App
+//
+//  Created by Fran Nasich on 10/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
